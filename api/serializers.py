54827b27d37c3e71227d3fe57bb0a83fd8c049a2
Mynul-07 <ahmedmynul18@gmail.com>
2025-05-22 11:07:12 +0600

    Add Student_code in serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from students.models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
+    student_code = serializers.SerializerMethodField()
+
     class Meta:
         model = Student
-        fields = "__all__"
-        # fields = ['student_id', 'name', 'branch']
+        fields = ['id', 'student_id', 'name', 'branch', 'student_code']
+
+    def get_student_code(self, obj):
+        return f"{obj.id}{obj.name}{obj.branch}"
