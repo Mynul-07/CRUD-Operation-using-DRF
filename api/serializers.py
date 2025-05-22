80156b06ff0e27af4c71a76ee893ff2d59af0d80
Mynul-07 <ahmedmynul18@gmail.com>
2025-05-22 15:47:22 +0600

    Add New App and try to use class based view

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from students.models import Student
+from employees.models import Employee
 
 class StudentSerializer(serializers.ModelSerializer):
     student_code = serializers.SerializerMethodField()
@@ -10,3 +11,9 @@ class StudentSerializer(serializers.ModelSerializer):
 
     def get_student_code(self, obj):
         return f"{obj.id}{obj.name}{obj.branch}"
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
