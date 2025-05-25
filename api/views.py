6717d6ca5a07157d4dea405d3717c858557e2585
Mynul-07 <ahmedmynul18@gmail.com>
2025-05-25 11:28:35 +0600

    solve the issues with put and delete options in mixins

--- api/views.py
+++ api/views.py
@@ -102,14 +102,15 @@ class Employees(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Generic
     def post(self, request):
         return self.create(request)
     
-class EmployeeDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin, generics.CreateAPIView):
+class EmployeeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     
     def get(self, request, pk):
         return self.retrieve(request, pk)
-        # retrieve the employee
     
     def put(self, request, pk):
         return self.update(request, pk)
     
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
